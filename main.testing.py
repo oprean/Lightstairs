84c19d4dde59054c5c7504ec10a5591348b32825
Sergiu Oprean <oprean@gmail.com>
2025-06-12 15:15:18 +0300

    7 trepte

--- main.testing.py
+++ main.testing.py
@@ -1,22 +1,27 @@
 import machine
 import neopixel
-from lib.ota import OTAUpdater
+from ota import OTAUpdater
 import time
 import urandom
 from constants import *
 
 # Configurația benzii LED
 pin = machine.Pin(0)  # Schimbă cu pinul tău de control
-num_leds = 30 + 29     # Total LED-uri pe ambele trepte
+num_leds = 30 + 29 + 29 + 29 + 29 + 29 + 29   # Total LED-uri pe ambele trepte
 np = neopixel.NeoPixel(pin, num_leds)
 firmware_url = "https://raw.githubusercontent.com/oprean/Lightstairs/master"
 
-ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.testing.py")
+ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.testing.py","main.py")
 ota_updater.download_and_install_update_if_available()
 
 # Definim intervalele pentru fiecare treaptă
 treapta1 = list(range(0, 30))    # LED-urile 0-29
 treapta2 = list(range(30, 59))   # LED-urile 30-58
+treapta3 = list(range(59, 88))   # LED-urile 30-58
+treapta4  = list(range(88, 117))   # LED-urile 30-58
+treapta5  = list(range(117, 146))   # LED-urile 30-58
+treapta6  = list(range(146, 175))   # LED-urile 30-58
+treapta7  = list(range(175, 204))   # LED-urile 30-58
 
 def culoare_random():
     return (urandom.getrandbits(8), urandom.getrandbits(8), urandom.getrandbits(8))
@@ -36,11 +41,22 @@ def aprinde_treapta(treapta):
 # Loop de test
 while True:
     aprinde_treapta(treapta1)
-    time.sleep(2)
+    time.sleep(1)
 
     aprinde_treapta(treapta2)
-    time.sleep(2)
-<<<<<<< HEAD
-=======
-
->>>>>>> ad5f536e46280e42d346d09173a3ed124afabfac
+    time.sleep(1)
+    
+    aprinde_treapta(treapta3)
+    time.sleep(1)
+    
+    aprinde_treapta(treapta4)
+    time.sleep(1)
+    
+    aprinde_treapta(treapta5)
+    time.sleep(1)
+    
+    aprinde_treapta(treapta6)
+    time.sleep(1)        
+    
+    aprinde_treapta(treapta7)
+    time.sleep(1)
